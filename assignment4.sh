deec79edaa66cd1ad1e04767adb8bbdcd4d36723
tooba001 <tooba.balooch@eurustechnologies.com>
2024-01-02 11:38:11 +0500

    my first project

--- assignment4.sh
+++ assignment4.sh
@@ -1,6 +1,12 @@
 #database setup
 #!/bin/bash
-sudo apt install mysql-server
+sudo apt-get update
+sudo apt-get install -y mysql-server mysql-client
+sudo mysql_secure_installation
 sudo systemctl start mysql
 sudo systemctl status mysql
-sudo sql -u root -p
+sudo mysql -u root -p
+mysql -u root -p -e "CREATE DATABASE wordpress_db;"
+mysql -u root -p -e "CREATE USER 'wordpress_user'@'%' IDENTIFIED BY 'tooba2001';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';"
+mysql -u root -p -e "FLUSH PRIVILEGES;"
