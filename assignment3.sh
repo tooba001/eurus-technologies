e0052fb35e2e7df3f54b6da3bd01e3132dde57db
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-02-20 02:48:33 -0500

    Delete assignment3.sh

--- assignment3.sh
+++
@@ -1,5 +0,0 @@
-#bash script that automatically fetches aws feed and stores them in a json file
-#!/bin/bash
-aws=$( curl -s "https://aws.amazon.com/blogs/aws/feed/" )
-echo "$aws" > output.json
-cat output.json
