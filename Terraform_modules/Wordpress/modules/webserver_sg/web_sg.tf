0df4c63fb366bd4d9eca900dbc1fbb4b4280767d
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-23 07:33:28 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/webserver_sg/web_sg.tf
+++ Terraform_modules/Wordpress/modules/webserver_sg/web_sg.tf
@@ -15,6 +15,7 @@ dynamic "ingress" {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
       security_groups = ingress.value.security_groups
     }
   }
