635c70f5e46ef0c20630a481b48300c58fb0cd50
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-23 07:30:41 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/db_sg/db_sg_variables.tf
+++ Terraform_modules/Wordpress/modules/db_sg/db_sg_variables.tf
@@ -6,6 +6,7 @@ variable "db_ingress_rules" {
     from_port   = number
     to_port     = number
     protocol    = string
+    cidr_blocks = list(string)
     security_groups = list(string)
   }))
 }
