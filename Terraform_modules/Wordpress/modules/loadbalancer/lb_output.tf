3734466ae1a436be44189fd1409d1611e1feea76
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-24 09:12:23 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/loadbalancer/lb_output.tf
+++ Terraform_modules/Wordpress/modules/loadbalancer/lb_output.tf
@@ -1,7 +1,3 @@
-output "launch_template_id" {
-  description = "ID of the VPC"
-  value       = aws_launch_template.wordpresslaunchtemplate.id
-}
 
 output "target_group_arns" {
   description = "ID of the VPC"
