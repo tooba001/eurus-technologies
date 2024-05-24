c2003c4dc97c9d08b91ff16b7c2158ee0ed5ec5a
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-24 09:14:28 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/loadbalancer/lb_variables.tf
+++ Terraform_modules/Wordpress/modules/loadbalancer/lb_variables.tf
@@ -8,34 +8,6 @@ variable "load_balancer_name" {
   type        = string
 }
 
-variable "instance_type" {
-  description = "The type of EC2 instance"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "ID of the Amazon Machine Image (AMI) to use for the EC2 instances"
-  type        = string
-  
-}
-
-
-variable "key_pair_name" {
-  description = "The name of the key pair to use for EC2 instances"
-  type        = string
-}
-
-variable "launch_template_name_prefix" {
-  description = "Prefix for the launch template name"
-  type        = string
-}
-
-variable "webserver_user_data_template" {
-  description = "Path to the web server user data template file"
-  type        = string
-  default     = "templates/webserver_user_data.tpl"  # Specify the path relative to the root directory
-}
-
 variable "vpc_id" {
   type = string
 }
@@ -48,26 +20,6 @@ variable "subnets" {
   type = list(string)
 } 
 
-variable "rds_endpoint" {
-  type = string
-}
 
-variable "security_group" {
-  type = list(string)
-}
+ 
 
-variable "subnet_id" {
-  type = string
-}   
-
-variable "db_password" {
-  description = "Database password"
-  type        = string
-  sensitive   = true  # Mark the variable as sensitive
-}
-
-variable "wp_db_password" {
-  description = "wordpress Database password"
-  type        = string
-  sensitive   = true  # Mark the variable as sensitive
-}
