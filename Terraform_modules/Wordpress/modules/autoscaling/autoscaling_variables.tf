702f475f0daf42b24ad43fb9b418c4c59c6327f2
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-24 09:16:13 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/autoscaling/autoscaling_variables.tf
+++ Terraform_modules/Wordpress/modules/autoscaling/autoscaling_variables.tf
@@ -23,14 +23,48 @@ variable "health_check_type" {
   type        = string
 }
 
-variable  "loadbalancer_launch_template_id" {
-  type = string
-} 
-
 variable "target_group_arns_id" {
   type = list(string)
  } 
 
 variable "vpc_zone_identifier" {
   type =  list(string)
+}
+
+variable  "rds_endpoint" {
+  type = string
+} 
+
+variable  "wp_db_password" {
+  type = string
+} 
+
+variable  "launch_template_name_prefix" {
+  type = string
+} 
+
+variable  "ami_id" {
+  type = string
+} 
+
+variable  "instance_type" {
+  type = string
+} 
+
+variable  "key_pair_name" {
+  type = string
+} 
+
+variable  "subnet_id" {
+  type = string
+} 
+
+variable  "security_groups" {
+  type = list(string)
+} 
+
+variable "webserver_user_data_template" {
+  description = "Path to the web server user data template file"
+  type        = string
+  default     = "templates/webserver_user_data.tpl"  # Specify the path relative to the root directory
 }
