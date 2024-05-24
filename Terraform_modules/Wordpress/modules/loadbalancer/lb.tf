d5481f5974edebe93a0d0c3f48f5cdd6d1db44ac
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-24 09:11:37 -0400

    Add files via upload

--- Terraform_modules/Wordpress/modules/loadbalancer/lb.tf
+++ Terraform_modules/Wordpress/modules/loadbalancer/lb.tf
@@ -38,33 +38,3 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-data "template_file" "webserveruser_data" {
-  template = file(var.webserver_user_data_template)
-
-   vars = {
-    rds_endpoint = var.rds_endpoint
-    wp_db_password = var.wp_db_password
-  }
-
-}
-
-resource "aws_launch_template" "wordpresslaunchtemplate" {
-  name_prefix   = var.launch_template_name_prefix
-  image_id      = var.ami_id  # Replace with your AMI ID
-  instance_type = var.instance_type
-  key_name      = var.key_pair_name
- 
-
-  # Specify the VPC configuration
-  network_interfaces {
-    associate_public_ip_address = true
-    subnet_id                   =  var.subnet_id # Choose your subnet ID
-    security_groups = var.security_group
-  }
-
-  user_data =  base64encode(data.template_file.webserveruser_data.rendered)
-              
-    tags = {
-    Name = "terraform-launch-template"
-  }        
-}
