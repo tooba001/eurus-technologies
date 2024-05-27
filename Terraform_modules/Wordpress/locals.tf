ad6a5e58c41c2524d49634ae167c9c125bd8c876
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-27 05:48:19 -0400

    Add files via upload

--- Terraform_modules/Wordpress/locals.tf
+++ Terraform_modules/Wordpress/locals.tf
@@ -6,7 +6,7 @@ locals {
       from_port      = 3306
       to_port        = 3306
       protocol       = "tcp"
-      cidr_blocks    =  [] 
+      cidr_blocks    = [] 
       security_groups = [module.websecuritygroups.webserver_securitygroup_id]
     }
   }
@@ -19,7 +19,7 @@ locals {
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = lookup(var.lb_ingress_rules.http, "cidr_blocks", ["0.0.0.0/0"])
     }
   }
 
@@ -29,7 +29,7 @@ locals {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks =   ["0.0.0.0/0"]
     }
     # Add more egress rules as needed
   }
@@ -38,20 +38,23 @@ locals {
 locals {
   web_ingress_rules = {
     http = {
-      description   = "Allow HTTP traffic"
-      from_port     = 80
-      to_port       = 80
-      protocol      = "tcp"
-      cidr_blocks   = [] 
-      security_groups = [module.lbsecuritygroups.lb_securitygroup_id]
+      description     = "Allow HTTP traffic"
+      from_port       = 80
+      to_port         = 80
+      protocol        = "tcp"
+      cidr_blocks     = [] 
+      security_groups = [module.securitygroups.lb_securitygroup_id]
     }
-  }
-  ssh = {
-      description   = "Allow SSH traffic"
-      from_port     =  22
-      to_port       =  22
-      protocol      =  "tcp"
-      cidr_blocks   = ["0.0.0.0/0"]
+    ssh = {
+      description     = "Allow SSH traffic"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
       security_groups = [] 
     }
   }
+}
+
+
+
