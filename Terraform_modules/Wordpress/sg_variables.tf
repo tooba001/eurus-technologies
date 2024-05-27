c49490f6d5eafa737ad6ac71e7617005d003c18b
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-27 08:10:01 -0400

    Add files via upload

--- Terraform_modules/Wordpress/sg_variables.tf
+++ Terraform_modules/Wordpress/sg_variables.tf
@@ -37,3 +37,17 @@ variable "db_ingress_rules" {
     security_groups = list(string)
   }))
 }
+
+variable "web_ingress_rules" {
+  description = "Map of ingress rules for the load balancer security group"
+  type = map(object({
+    description      =     string
+    from_port        =     number
+    to_port          =     number
+    protocol         =     string
+    cidr_blocks      =     list(string)
+    security_groups  =     list(string)
+  }))
+
+}
+
