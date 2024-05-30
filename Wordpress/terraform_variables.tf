4638ab348a8172e15502bbe8951de135699735e6
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-30 07:13:52 -0400

    Add files via upload

--- Wordpress/terraform_variables.tf
+++ Wordpress/terraform_variables.tf
@@ -124,12 +124,16 @@ variable "wp_db_password" {
   sensitive   = true  # Mark the variable as sensitive
 }
 
-variable "lb_ingress_rules" {
+variable "ingress_rules" {
   type = map(object({
     description = string
     from_port   = number
     to_port     = number
     protocol    = string
-    cidr_blocks = list(string)
+    cidr_blocks = optional(list(string), [])
+    security_groups = optional(list(string), [])
   }))
 }
+
+
+
