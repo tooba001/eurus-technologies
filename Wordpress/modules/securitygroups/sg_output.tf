53c0590d7aa9bc000c2668ea9b9db3c2813fdc0c
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-30 07:15:20 -0400

    Add files via upload

--- Wordpress/modules/securitygroups/sg_output.tf
+++ Wordpress/modules/securitygroups/sg_output.tf
@@ -1,13 +1,7 @@
-output "rds_securitygroup_id" {
-    value= aws_security_group.rds_sg.id
+output "securitygroup_id" {
+    value= aws_security_group.shared_security_group.id
 }
 
-output "lb_securitygroup_id" {
-  description = "ID of the VPC"
-  value       = aws_security_group.alb_sg.id
-}
 
-output "webserver_securitygroup_id" {
-  description = "ID of the VPC"
-  value       = aws_security_group.web_sg.id
-}
+
+
