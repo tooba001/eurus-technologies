f7cb8d1142a4a750569b11d7af78c40d5eff975e
tooba001 <155063385+tooba001@users.noreply.github.com>
2024-05-30 07:14:38 -0400

    Add files via upload

--- Wordpress/modules/securitygroups/sg.tf
+++ Wordpress/modules/securitygroups/sg.tf
@@ -1,11 +1,12 @@
 
-resource "aws_security_group" "alb_sg" {
-  name        = "alb_sg"
-  description = "Allow inbound traffic on ports 80 "
+resource "aws_security_group" "shared_security_group" {
+  name        = var.security_group_name 
+  description = "Allow  traffic "
   vpc_id      =  var.vpc_id
 
+
   dynamic "ingress" {
-    for_each = var.lb_ingress_rules
+    for_each = var.ingress_rules
 
 
     content {
@@ -13,88 +14,26 @@ resource "aws_security_group" "alb_sg" {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
-      cidr_blocks = length(lookup(ingress.value, "cidr_blocks", [])) > 0 ? lookup(ingress.value, "cidr_blocks", []) : ["0.0.0.0/0"]
+      cidr_blocks = lookup(ingress.value, "cidr_blocks", null)
+      security_groups =  lookup(ingress.value, "security_groups", null)
     }
   }
 
  dynamic "egress" {
-    for_each = var.lb_egress_rules
+    for_each =  var.egress_rules
 
     content {
       description = egress.value.description
       from_port   = egress.value.from_port
       to_port     = egress.value.to_port
       protocol    = egress.value.protocol
-      cidr_blocks = length(lookup(egress.value, "cidr_blocks", [])) > 0 ? lookup(egress.value, "cidr_blocks", []) : ["0.0.0.0/0"]
+      cidr_blocks =  lookup(egress.value, "cidr_blocks", null)
+      security_groups = lookup(egress.value, "security_groups", null)
     }
   }
 }
 
 
-resource "aws_security_group" "rds_sg" {
-  name        = "rds_sg"
-  description = "Allow inbound traffic from EC2"
-  vpc_id      = var.vpc_id
 
-  dynamic "ingress" {
-    for_each = var.db_ingress_rules
-
-    content {
-      description     = ingress.value.description
-      from_port       = ingress.value.from_port
-      to_port         = ingress.value.to_port
-      protocol        = ingress.value.protocol
-      cidr_blocks     = ingress.value.cidr_blocks                   
-      security_groups = ingress.value.security_groups
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.db_egress_rules
-
-    content {
-      description = egress.value.description
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-}
-
-
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Security group for web servers"
-
-  vpc_id = var.vpc_id
-
-dynamic "ingress" {
-    for_each = var.web_ingress_rules
-
-
-    content {
-      description = ingress.value.description
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-      security_groups = ingress.value.security_groups
-    }
-  }
-
-    dynamic "egress" {
-    for_each = var.web_egress_rules
-
-    content {
-      description = egress.value.description
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-
-}
 
 
